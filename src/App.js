1810cf66422a6cfe1a414bdce7b0586a90594d54
unknown <dmitrivostap8@gmail.com>
2022-02-13 09:54:17 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.scss';
-import Layout from './components/Layout';
+import Header from './components/Header'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <Layout>
-      <h1>Test</h1>
-    </Layout>
+    <>
+      <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
